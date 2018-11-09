17a57d5821a5d8a984df4598e70b482167a9428d
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-11-09 13:02:21 +0100

    GREEN: HomeController Delivers a View result

---
+++ 2dv610-TDD/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace _2dv610_TDD.Controllers
+{
+    public class HomeController : Controller
+{
+    // GET: /<controller>/
+    public ViewResult Index()
+    {
+        return View();
+    }
+}
+}
--- XUnitTest/HomeControllerTest.cs
+++ XUnitTest/HomeControllerTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
+using _2dv610_TDD.Controllers;
 
 namespace XUnitTest
 {

e6c629d914fdb337afb61b992ffe223189b40291
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-11-13 13:54:57 +0100

    RED: UsernameValidationTest

---
+++ 2dv610-TDD/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2dv610_TDD.Models
+{
+    public class User
+{
+    public User(string username, string password)
+    {
+        
+    }
+}
+}
---
+++ XUnitTest/UserTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Xunit;
+using _2dv610_TDD.Models;
+
+namespace XUnitTest
+{
+    public class UserTest
+    {
+        [Fact]
+        public void UsernameWithLessThen3CharsShouldThrowError()
+        {
+            User Sut = new User("q", "ValidPassword");
+            bool validate = Validator.TryValidateObject(Sut, new ValidationContext(Sut), null, true);
+
+            Assert.False(validate);
+        }
+    }
+}

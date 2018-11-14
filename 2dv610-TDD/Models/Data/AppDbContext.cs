d6112a873841c02179b0f2ca04576489742dce48
Fredrik <fredrik@local>
2018-11-14 13:03:26 +0100

    Added Db Support

---
+++ 2dv610-TDD/Models/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2dv610_TDD.Models.Data
+{
+    public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions options) 
+        : base(options)
+    {
+        
+    }
+}
+}
--- 2dv610-TDD/Startup.cs
+++ 2dv610-TDD/Startup.cs
@@ -5,16 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using _2dv610_TDD.Models;
+using _2dv610_TDD.Models.Data;
 
 namespace _2dv610_TDD
 {
     public class Startup
     {
+        private IConfiguration Congiguration { get; set; }
+        public Startup(IConfiguration congiguration )
+        {
+            Congiguration = congiguration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Congiguration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
             services.AddMvc();
         }
 
@@ -27,6 +42,7 @@ namespace _2dv610_TDD
             }
 
             app.UseStaticFiles();
+            app.UseMvc();
             app.UseMvcWithDefaultRoute();
         }
     }

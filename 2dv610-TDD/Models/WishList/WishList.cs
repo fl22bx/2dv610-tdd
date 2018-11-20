6f51f2f5cdf2d7aea14388244c18a0e0d5e07ab3
Fredrik <fredrik@local>
2018-11-20 12:04:57 +0100

    RED: WishListTest Should be able to add wishes

---
+++ 2dv610-TDD/Models/WishList/WishList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace _2dv610_TDD.Models.WishList
+{
+    public class WishList
+{
+    public List<string> GetWishList { get; set; }
+    public WishList()
+    {
+        
+    }
+
+    public void AddWish(string wish)
+    {
+
+    }
+}
+}
---
+++ XUnitTest/WishListTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using _2dv610_TDD.Models.WishList;
+
+namespace XUnitTest
+{
+    public class WishListTest
+    {
+        [Fact]
+        public void ShouldReturnListWithWishes()
+        {
+            WishList Sut  = new WishList();
+
+            Sut.AddWish("Wish1");
+            Sut.AddWish("Wish2");
+
+            List<string> Actual = Sut.GetWishList;
+
+            Assert.Equal(2 , Actual.Count);
+
+        }
+    }
+
+}

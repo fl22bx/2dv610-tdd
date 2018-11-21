a61af85d49257f1c14cdbaf5507f60d12057d7b2
Fredrik <fredrik@local>
2018-11-21 10:45:46 +0100

    RED: WishListViewModel need test

---
+++ XUnitTest/WishListViewModelTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Xunit;
+using _2dv610_TDD.Models.WishList;
+using _2dv610_TDD.ViewModels;
+
+namespace XUnitTest
+{
+    class WishListViewModelTest
+    {
+        public WishListVieModel Sut { get; set; }
+        public WishListViewModelTest()
+        {
+            Sut = new WishListVieModel();
+
+            var wishListMock = Mock.Of<WishList>();
+            Sut.NeedWishes = wishListMock;
+        }
+
+        public void NeedWishListNotNull()
+        {
+            Assert.NotNull(Sut.NeedWishes);
+            
+        }
+    }
+}
